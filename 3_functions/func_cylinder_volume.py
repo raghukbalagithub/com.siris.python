135af2fc4afa49483da905e2300291af3a32c893
Raghu Prashanth KB <raghukbala@gmail.com>
2026-04-28 09:23:07 -0700

    20260428 - project restructuring

--- 3_functions/func_cylinder_volume.py
+++ 3_functions/func_cylinder_volume.py
@@ -2,6 +2,11 @@ radius = int(input("Radius: "))
 height = int(input("Height: "))
 
 def cylinder_volume(radius, height):
+    '''
+    :param radius: Radius of cylinder
+    :param height: Height of cylinder
+    :return: Volume of cylinder
+    '''
     volume = (radius**2) * 3.14 * height
     return volume
 
